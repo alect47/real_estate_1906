d861acc10d9ddbd9476309d266eb859367d4fdcf
Alec Wells <alec.wells47@gmail.com>
2019-07-03 09:17:45 -0600

    Finish iteration 1

--- test/room_test.rb
+++ test/room_test.rb
@@ -16,4 +16,9 @@ class RoomTest < Minitest::Test
     assert_equal :bedroom, room.category
   end
 
+  def test_it_has_area
+    room = Room.new(:bedroom, 10, 13)
+
+    assert_equal 130, room.area
+  end
 end
