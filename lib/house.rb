b24a1efbd9d01c81f55f1c9f1a28a69154c16eb4
Alec Wells <alec.wells47@gmail.com>
2019-07-03 10:58:53 -0600

    Finish iteration 4

--- lib/house.rb
+++ lib/house.rb
@@ -1,11 +1,13 @@
 class House
-  attr_reader :price, :address, :rooms, :area
+  attr_reader :price, :address, :rooms, :area, :rooms_by_area, :rooms_by_category
 
-  def initialize(price, address, rooms = [], area = 0)
+  def initialize(price, address, rooms = [], area = 0, rooms_by_area = [], rooms_by_category = {})
     @price = price
     @address = address
     @rooms = rooms
     @area = area
+    @rooms_by_area = rooms_by_area
+    @rooms_by_category = rooms_by_category
   end
 
   def add_room(room)
@@ -23,6 +25,40 @@ class House
       @area += room.area
     end
     @area
+  end
+
+  def price_per_square_foot
+    price = @price[1..-1].to_f
+    (price / area).round(2)
+  end
+
+  # def rooms_by_area
+  #   @room_areas = @rooms.map do |room|
+  #     room.area
+  #   end.sort.reverse
+  #
+  #   @rooms.each do |room|
+  #     room_areas.each do |area|
+  #       if @rooms.area == area
+  #         @rooms_by_area << room
+  #       end
+  #   end
+  #   @rooms_by_area
+  # end
+
+  def rooms_by_area
+    @rooms.sort_by { |room| room.area}.reverse
+  end
+
+  def rooms_by_category
+    #@rooms_by_category = @rooms.sort_by { |room| room.category}
+    @rooms.each do |room|
+      if !@rooms_by_category.keys.include?(room.category)
+        @rooms_by_category[room.category] = []
+      end
+        @rooms_by_category[room.category] << room
+    end
+    @rooms_by_category
   end
 
 
