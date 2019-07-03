2d658d5560457e81154f2e3b95875046f2cdc166
Alec Wells <alec.wells47@gmail.com>
2019-07-03 09:49:23 -0600

    Finish Iteration 3

--- lib/house.rb
+++ lib/house.rb
@@ -1,14 +1,28 @@
 class House
-  attr_reader :price, :address, :rooms
+  attr_reader :price, :address, :rooms, :area
 
-  def initialize(price, address, rooms = [])
+  def initialize(price, address, rooms = [], area = 0)
     @price = price
     @address = address
     @rooms = rooms
+    @area = area
   end
 
   def add_room(room)
     @rooms << room
+  end
+
+  def rooms_from_category(category)
+    @rooms.find_all do |room|
+      room.category == category
+    end
+  end
+
+  def area
+    @rooms.each do |room|
+      @area += room.area
+    end
+    @area
   end
 
 
--- test/house_test.rb
+++ test/house_test.rb
@@ -36,9 +36,38 @@ class HouseTest < Minitest::Test
     room_2 = Room.new(:bedroom, 11, 15)
     house.add_room(room_1)
     house.add_room(room_2)
-    binding.pry
 
     assert_equal [room_1, room_2], house.rooms
+  end
+
+  def test_can_find_rooms_from_category
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, 13)
+    room_2 = Room.new(:bedroom, 11, 15)
+    room_3 = Room.new(:living_room, 25, 15)
+    room_4 = Room.new(:basement, 30, 41)
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+
+
+    assert_equal [room_1, room_2], house.rooms_from_category(:bedroom)
+    assert_equal [room_4], house.rooms_from_category(:basement)
+  end
+
+  def test_it_can_find_total_area
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, 13)
+    room_2 = Room.new(:bedroom, 11, 15)
+    room_3 = Room.new(:living_room, 25, 15)
+    room_4 = Room.new(:basement, 30, 41)
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+
+    assert_equal 1900, house.area
   end
 
 end
