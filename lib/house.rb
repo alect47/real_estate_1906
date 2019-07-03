280b6624ac21cea1b50bdb688d640894e19ec0af
Alec Wells <alec.wells47@gmail.com>
2019-07-03 09:27:35 -0600

    Finish iteration 2

---
+++ lib/house.rb
@@ -0,0 +1,15 @@
+class House
+  attr_reader :price, :address, :rooms
+
+  def initialize(price, address, rooms = [])
+    @price = price
+    @address = address
+    @rooms = rooms
+  end
+
+  def add_room(room)
+    @rooms << room
+  end
+
+
+end
